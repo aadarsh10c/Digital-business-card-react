d29fb82e9871c1331d41b9e1b241aae5f811ea1b
Adarsh Kumar <adarsh010c@gmail.com>
2023-01-19 01:51:39 +0530

    Added buttons along with its style

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -4,7 +4,22 @@ import React from "react";
 export default function Main (){
     return (
         <main className="content">
-            Main is here
+            <div className="content__title">
+                <h3 className="content__title-main">Laura Smith</h3>
+                <h5 className="content__title-sub">Frontend Developer</h5>
+                <h6 className="content__title-email">laurasmith.website</h6>               
+            </div>
+            <div className="content__link">
+                <a href="#" className="content__link-1">
+                <span><i class="fa-solid fa-envelope "></i></span>
+                    Email
+                </a>
+                <a href="#" className="content__link-2">
+                    <span><i class="fa-brands fa-linkedin "></i></span>
+                    Linkedin
+                </a>
+            </div>
+            <div className="content__info"></div>
         </main>
     );
 }
