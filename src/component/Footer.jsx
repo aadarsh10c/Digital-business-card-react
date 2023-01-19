333039177963f1753d47b51295931582cd0bfb33
Adarsh Kumar <adarsh010c@gmail.com>
2023-01-19 15:23:33 +0530

    Completed footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,20 @@ import React from "react";
 export default function Footer (){
     return (
         <footer className="footer">
-            Footer done !
+            <div className="footer__links">
+                <div className="footer__icon">
+                    <i class="fa-brands fa-square-twitter"></i>
+                </div>
+                <div className="footer__icon">
+                    <i class="fa-brands fa-square-facebook"></i>
+                </div>
+                <div className="footer__icon">
+                    <i class="fa-brands fa-square-instagram"></i>
+                </div>
+                <div className="footer__icon">
+                    <i class="fa-brands fa-square-github"></i>
+                </div>
+            </div>
         </footer>
     )
 }
